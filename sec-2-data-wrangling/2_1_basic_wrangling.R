630518679c3366e1955086df1183c72d403bb6ec
Kiran Sankar Das <kiran.sankar.das@gmail.com>
2025-03-05 20:45:27 +0530

    Refactor data wrangling script to include new filtering and selection methods for murder rates

--- sec-2-data-wrangling/2_1_basic_wrangling.R
+++ sec-2-data-wrangling/2_1_basic_wrangling.R
@@ -4,6 +4,14 @@ data("murders")
 
 # adding a column with mutate
 murders  <- mutate(murders, rate = total / population * 100000)
+head(murders)
 
 # subsetting with filter
 filter(murders, rate <= 0.71)
+
+# selecting columns with select
+new_table <- select(murders, state, region, rate)
+filter(new_table, rate <= 0.71)
+
+# using pipe
+murders %>% select(state, region, rate) %>% filter(rate <= 0.71)
