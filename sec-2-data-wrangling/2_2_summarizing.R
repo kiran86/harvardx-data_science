4b61470e7643069f2fee80b528bcaf1e19ec0577
Kiran Sankar Das <kiran.sankar.das@gmail.com>
2025-03-06 06:19:46 +0530

    Add sorting and ranking functions for murder rates by region and overall

--- sec-2-data-wrangling/2_2_summarizing.R
+++ sec-2-data-wrangling/2_2_summarizing.R
@@ -82,3 +82,12 @@ murders %>% arrange(rate) %>% head()
 
 # # order the states by murder rate in descending order
 murders %>% arrange(desc(rate)) %>% head()
+
+# order the states by region and then by murder rate within region
+murders %>% arrange(region, rate) %>% head()
+
+# return the top 10 states by murder rate
+murders %>% top_n(10, rate)
+
+# return the top 10 states ranked by murder rate, sorted by murder rate
+murders %>% arrange(desc(rate)) %>% top_n(10)
